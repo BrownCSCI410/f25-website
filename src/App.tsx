e387e3df757aaf62a5f60d41277b4879121eb075
Eric <eric_ewing@brown.edu>
2025-09-01 14:04:09 -0400

    add basename wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Navbar } from './Components/NavBar/Navbar';
 import { Welcome } from './Components/Welcome/Welcome';
 import { Assignments } from './Components/Assignments/Assignments';
@@ -7,29 +8,30 @@ import { Labs } from './Components/Labs and Sections/Labs';
 import { Hours } from './Components/Hours/Hours';
 import { Resources } from './Components/Resources/Resources';
 import { Staff } from './Components/Staff/Staff';
-// import { TournamentResults } from './Components/TournamentResults/TournamentResults';
-import './App.scss'
-import { Route, Routes } from 'react-router-dom';
 import { Glossary } from './Components/Glossary/glossary';
+import './App.scss'
 
 export default function App() {
+  // Set basename for GitHub Pages deployment
+  const basename = process.env.NODE_ENV === 'production' ? '/f25-website' : '';
+  
   return (
-    <div className="App">
-      <Navbar />
-      <main className="main-container">
-        <Routes>
-          <Route path="/" element={<Welcome />} />
-          <Route path="/labs" element={<Labs />} />
-          <Route path="/lectures" element={<Lectures />} />
-          <Route path="/assignments" element={<Assignments />} />
-          <Route path="/resources" element={<Resources />} />
-          <Route path="/hours" element={<Hours />} />
-          <Route path="/staff" element={<Staff />} />
-          <Route path="/glossary" element={<Glossary />} />
-          
-        </Routes>
-      </main>
-    </div>
+    <BrowserRouter basename={basename}>
+      <div className="App">
+        <Navbar />
+        <main className="main-container">
+          <Routes>
+            <Route path="/" element={<Welcome />} />
+            <Route path="/labs" element={<Labs />} />
+            <Route path="/lectures" element={<Lectures />} />
+            <Route path="/assignments" element={<Assignments />} />
+            <Route path="/resources" element={<Resources />} />
+            <Route path="/hours" element={<Hours />} />
+            <Route path="/staff" element={<Staff />} />
+            <Route path="/glossary" element={<Glossary />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
-
